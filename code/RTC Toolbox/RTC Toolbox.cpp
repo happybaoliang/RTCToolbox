8f336c44dd80c0009981f2619ccd82a75381ac80
happy baoliang <happybaoliang@gmail.com>
2014-04-17 14:25:16 -0400

    20140417

--- code/RTC Toolbox/RTC Toolbox.cpp
+++ code/RTC Toolbox/RTC Toolbox.cpp
@@ -14,19 +14,11 @@ using namespace std;
 #define Warning(...)    fprintf(stderr, __VA_ARGS__)
 
 
-unsigned int* output;
-unsigned int* cendx1;
-unsigned int* cendx2;
-unsigned int* cvalue1;
-unsigned int* cvalue2;
-unsigned int* cstartx1;
-unsigned int* cstartx2;
-
-
 struct Opts {
 	const char* compiler_option;
 	cl_device_type device_type;
-	unsigned int array_size;
+	unsigned int array_size1;
+	unsigned int array_size2;
 	const char *kernelName;
 	const char *fileName;
 	bool fromfile;
@@ -43,19 +35,44 @@ struct CLGoo {
 };
 
 
+struct point {
+	float startx;
+	float starty;
+	float midx;
+	float midy;
+	float endx;
+	float endy;
+};
+
+
+struct point* output=NULL;
+struct point* operand1=NULL;
+struct point* operand2=NULL;
+
+
 const char* kernelSourceCode=KERNEL(
-	__kernel void computemap(__global uint* startx1,__global uint* endx1,__global uint* y1,__global uint* startx2,
-		__global uint* endx2,__global uint* y2,__global uint* map){
-	size_t xId=get_global_id(0);
-	size_t yId=get_global_id(1);
+	typedef struct {
+		float startx;
+		float starty;
+		float midx;
+		float midy;
+		float endx;
+		float endy;
+	}point;
 
-	uint val=y1[xId]+y2[yId];
-	uint endx=endx1[xId]+endx2[yId];
-	uint startx=startx1[xId]+startx2[yId];
+	__kernel void computemap(__global point* operand1,__global point* operand2,__global int stage,__global point* output){
+		size_t size=get_global_size(0);
+		size_t col_idx=get_global_id(1);
+		size_t row_idx=get_global_id(0);
+		size_t col_offset=get_global_offset(1);
+		size_t row_offset=get_global_offset(0);
+		size_t index=(row_idx-row_offset)*size+(col_idx-col_offset);
 
-	for (int i=startx;i<=endx;i++)
-		atomic_min(map+i,val);
-}
+		output[index].startx=col_idx;
+		output[index].starty=row_idx;
+		output[index].endx=col_offset;
+		output[index].endy=row_offset;
+	}
 );
 
 
@@ -79,12 +96,12 @@ static struct { cl_int code; const char *msg; } error_table[] = {
 };
 
 
-static void DumpBuffer(const Opts* opts, const unsigned int *buffer){
-	for (size_t ii = 0; ii < 2*opts->array_size; ii++) {
+static void DumpBuffer(const Opts* opts, const point* buffer){
+	for (size_t ii = 0; ii < opts->array_size1*opts->array_size2; ii++) {
 		if (ii % 10 == 0) {
-			printf("\n%5d:", ii);
+			printf("\n%4d:", ii);
 		}
-		printf(" %5d", buffer[ii]);
+		printf("(%.0f,%.0f) ", buffer[ii].startx,buffer[ii].starty);
 	}
 	printf("\n");
 }
@@ -97,7 +114,6 @@ static const char * StrCLError(cl_int status) {
 			return error_table[ii].msg;
 		}
 	}
-
 	sprintf_s(unknown, "unknown error %d\n", status);
 	return unknown;
 }
@@ -125,56 +141,45 @@ static int InitializeCL(CLGoo *goo, const Opts *opts){
 	size_t platformNum;
 	cl_platform_id* platforms;
 	cl_int status;
-
 	memset(goo, 0, sizeof(*goo));
-
 	status=clGetPlatformIDs(0,NULL,&platformNum);
 	if(status!=CL_SUCCESS){
 		Warning("unable to get platform number: %d\n",status);
 		return -1;
 	}
-
 	platforms=(cl_platform_id*)malloc(platformNum*sizeof(cl_platform_id));
-
 	status=clGetPlatformIDs(platformNum,platforms,NULL);
 	if (status!=CL_SUCCESS){
 		Warning("unable to get platform: %d\n",status);
 		return -1;
 	}
-
 	cl_context_properties prop[] = {CL_CONTEXT_PLATFORM,(cl_context_properties)platforms[0],0};
-
 	goo->ctx = clCreateContextFromType(prop, opts->device_type,HandleCLError, NULL, &status);
 	if (status != CL_SUCCESS) {
 		Warning("clCreateContextFromType failed: %s\n", StrCLError(status));
 		return 0;
 	}
-
 	status = clGetContextInfo(goo->ctx,CL_CONTEXT_DEVICES, 0, NULL, &deviceListSize);
 	if (status != CL_SUCCESS) {
 		Warning("clGetContextInfo failed: %s\n", StrCLError(status));
 		return 0;
 	}
 	goo->numDevices = deviceListSize / sizeof(cl_device_id);
-
 	if ((goo->devices = (cl_device_id *) malloc(deviceListSize)) == NULL) {
 		Warning("Failed to allocate memory (deviceList).\n");
 		return 0;
 	}
-
 	status = clGetContextInfo(goo->ctx, CL_CONTEXT_DEVICES, deviceListSize,goo->devices, NULL);
 	if (status != CL_SUCCESS) {
 		Warning("clGetGetContextInfo failed: %s\n", StrCLError(status));
 		return 0;
 	}
-
 	queueProps = opts->timing ? CL_QUEUE_PROFILING_ENABLE : 0;
 	goo->queue = clCreateCommandQueue(goo->ctx,goo->devices[0], queueProps, &status);
 	if (status != CL_SUCCESS) {
 		Warning("clCreateCommandQueue failed: %s\n", StrCLError(status));
 		return 0;
 	}
-
 	return 1;
 }
 
@@ -204,17 +209,15 @@ static const char * FileToString(const Opts* opts) {
 
 
 static int LoadKernel(const Opts* opts, cl_kernel *kernel, CLGoo *goo) {
+	cl_int status;
+	cl_program program;
 	const char *source = FileToString(opts);
 	size_t sourceSize[] = {strlen(source)};
-	cl_program program;
-	cl_int status;
-
 	program = clCreateProgramWithSource(goo->ctx, 1,&source, sourceSize, &status);
 	if (status != CL_SUCCESS) {
 		Warning("clCreateProgramWithSource failed: %s", StrCLError(status));
 		return 0;
 	}
-
 	status = clBuildProgram(program, 1, goo->devices, opts->compiler_option, NULL, NULL);
 	if (status != CL_SUCCESS) {
 		size_t log_size;
@@ -227,17 +230,14 @@ static int LoadKernel(const Opts* opts, cl_kernel *kernel, CLGoo *goo) {
 		free(program_log);
 		return 0;
 	}
-
 	*kernel = clCreateKernel(program, opts->kernelName, &status);
 	if (status != CL_SUCCESS) {
 		Warning("clCreateKernel(%s) failed: %s\n",opts->kernelName, StrCLError(status));
 		return 0;
 	}
-
 	if (clReleaseProgram(program) != CL_SUCCESS) {
 		Warning("clReleaseProgram() failed.  Bummer.\n");
 	}
-
 	return 1;
 }
 
@@ -246,81 +246,60 @@ static int RunTest(CLGoo *goo, const Opts *opts) {
 	cl_int status;
 	cl_event event;
 	cl_kernel kernel;
-	size_t global_work_size[2]={opts->array_size,opts->array_size};
-
+	size_t global_work_size[2]={opts->array_size1,opts->array_size2};
 	if (!LoadKernel(opts,&kernel,goo)) {
 		return 0;
 	}
-
-	cl_mem endx1=clCreateBuffer(goo->ctx,CL_MEM_COPY_HOST_PTR,opts->array_size*sizeof(unsigned int),cendx1,&status);
+	cl_mem input1=clCreateBuffer(goo->ctx,CL_MEM_COPY_HOST_PTR,opts->array_size1*sizeof(struct point),operand1,&status);
 	if(status!=CL_SUCCESS){
 		printf("Error: clCreateBuffer 1.\n");
 		return EXIT_FAILURE;
 	}
-	cl_mem endx2=clCreateBuffer(goo->ctx,CL_MEM_COPY_HOST_PTR,opts->array_size*sizeof(unsigned int),cendx2,&status);
+	cl_mem input2=clCreateBuffer(goo->ctx,CL_MEM_COPY_HOST_PTR,opts->array_size2*sizeof(struct point),operand2,&status);
 	if(status!=CL_SUCCESS){
 		printf("Error: clCreateBuffer 2.\n");
 		return EXIT_FAILURE;
 	}
-	cl_mem value1=clCreateBuffer(goo->ctx,CL_MEM_COPY_HOST_PTR,opts->array_size*sizeof(unsigned int),cvalue1,&status);
+	cl_mem outputBuffer=clCreateBuffer(goo->ctx,CL_MEM_WRITE_ONLY,opts->array_size1*opts->array_size2*sizeof(struct point),NULL,&status);
 	if(status!=CL_SUCCESS){
 		printf("Error: clCreateBuffer 3.\n");
 		return EXIT_FAILURE;
 	}
-	cl_mem value2=clCreateBuffer(goo->ctx,CL_MEM_COPY_HOST_PTR,opts->array_size*sizeof(unsigned int),cvalue2,&status);
+	status=clSetKernelArg(kernel,0,sizeof(cl_mem),(void*)&input1);
 	if(status!=CL_SUCCESS){
-		printf("Error: clCreateBuffer 4.\n");
+		printf("Error: clSetKernelArg 0.\n");
 		return EXIT_FAILURE;
 	}
-	cl_mem startx1=clCreateBuffer(goo->ctx,CL_MEM_COPY_HOST_PTR,opts->array_size*sizeof(unsigned int),cstartx1,&status);
-	if(status!=CL_SUCCESS){
-		printf("Error: clCreateBuffer 5.\n");
-		return EXIT_FAILURE;
-	}
-	cl_mem startx2=clCreateBuffer(goo->ctx,CL_MEM_COPY_HOST_PTR,opts->array_size*sizeof(unsigned int),cstartx2,&status);
-	if(status!=CL_SUCCESS){
-		printf("Error: clCreateBuffer 6.\n");
-		return EXIT_FAILURE;
-	}
-	cl_mem outputBuffer=clCreateBuffer(goo->ctx,CL_MEM_READ_ONLY,2*opts->array_size*sizeof(unsigned int),NULL,&status);
-	if(status!=CL_SUCCESS){
-		printf("Error: clCreateBuffer 7.\n");
-		return EXIT_FAILURE;
-	}
-
-	status=clSetKernelArg(kernel,0,sizeof(cl_mem),(void*)&startx1);
-	if(status!=CL_SUCCESS){
+	status=clSetKernelArg(kernel,1,sizeof(cl_uint),(void*)&(opts->array_size1));
+	if (status!=CL_SUCCESS){
 		printf("Error: clSetKernelArg 1.\n");
 		return EXIT_FAILURE;
 	}
-	status=clSetKernelArg(kernel,1,sizeof(cl_mem),(void*)&endx1);
+	status=clSetKernelArg(kernel,2,sizeof(cl_mem),(void*)&input2);
 	if(status!=CL_SUCCESS){
 		printf("Error: clSetKernelArg 2.\n");
 		return EXIT_FAILURE;
 	}
-	status=clSetKernelArg(kernel,2,sizeof(cl_mem),(void*)&value1);
-	if(status!=CL_SUCCESS){
+	status=clSetKernelArg(kernel,3,sizeof(cl_uint),(void*)&(opts->array_size2));
+	if (status!=CL_SUCCESS){
 		printf("Error: clSetKernelArg 3.\n");
 		return EXIT_FAILURE;
 	}
-	status=clSetKernelArg(kernel,3,sizeof(cl_mem),(void*)&startx2);
-	if(status!=CL_SUCCESS){
-		printf("Error: clSetKernelArg 4.\n");
-		return EXIT_FAILURE;
-	}
-	status=clSetKernelArg(kernel,4,sizeof(cl_mem),(void*)&endx2);
+	status=clSetKernelArg(kernel,5,sizeof(cl_mem),(void*)&outputBuffer);
 	if(status!=CL_SUCCESS){
 		printf("Error: clSetKernelArg 5.\n");
 		return EXIT_FAILURE;
 	}
-	status=clSetKernelArg(kernel,5,sizeof(cl_mem),(void*)&value2);
-	if(status!=CL_SUCCESS){
-		printf("Error: clSetKernelArg 6.\n");
-		return EXIT_FAILURE;
-	}
-	status=clSetKernelArg(kernel,6,sizeof(cl_mem),(void*)&outputBuffer);
-	if(status!=CL_SUCCESS){
-		printf("Error: clSetKernelArg 7.\n");
+	cl_uint stage=0;
+	//for (int stage=0;stage<opts->array_size2;stage++){
+		status=clSetKernelArg(kernel,4,sizeof(cl_uint),(void*)&stage);
+		if(status!=CL_SUCCESS){
+			printf("Error: clSetKernelArg 4.\n");
+			return EXIT_FAILURE;
+		}
+	//}
+	if (status!=CL_SUCCESS){
+		printf("Error: clSetKernelArg 3.\n");
 		return EXIT_FAILURE;
 	}
 
@@ -329,16 +308,13 @@ static int RunTest(CLGoo *goo, const Opts *opts) {
 		Warning("Failed to launch kernel: %s\n", StrCLError(status));
 		return 0;
 	}
-
 	if ((status = clWaitForEvents(1, &event)) != CL_SUCCESS) {
 		Warning("clWaitForEvents() failed: %s\n", StrCLError(status));
 		clFinish(goo->queue); 
 	}
-
 	if (opts->timing) {
 		double total;
 		long long start, end;
-
 		status = clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START,sizeof(start), &start, NULL);
 		if (status != CL_SUCCESS) {
 			Warning("clGetEventProfilingInfo(COMMAND_START) failed: %s\n",StrCLError(status));
@@ -349,48 +325,36 @@ static int RunTest(CLGoo *goo, const Opts *opts) {
 			Warning("clGetEventProfilingInfo(COMMAND_END) failed: %s\n",StrCLError(status));
 			end = 0;
 		}
-
 		total = (double)(end - start) / 1e6;
 		printf("Profiling: Total kernel time was %5.2f msecs.\n", total);
 	}
-
-	status=clEnqueueReadBuffer(goo->queue,outputBuffer,CL_TRUE,0,2*opts->array_size,output,0,NULL,NULL);
+	status=clEnqueueReadBuffer(goo->queue,outputBuffer,CL_TRUE,0,opts->array_size1*opts->array_size2*sizeof(struct point),output,0,NULL,NULL);
 	if (status!=CL_SUCCESS){
 		printf("Error: clEnqueueReadBuffer.\n");
 		return EXIT_FAILURE;
 	}
-
 	if (!opts->quiet){
 		DumpBuffer(opts,output);
 	}
-
 	clReleaseEvent(event);
 	status=clReleaseKernel(kernel);
-	status=clReleaseMemObject(endx1);
-	status=clReleaseMemObject(endx2);
-	status=clReleaseMemObject(value1);
-	status=clReleaseMemObject(value2);
-	status=clReleaseMemObject(startx1);
-	status=clReleaseMemObject(startx2);
+	status=clReleaseMemObject(input1);
+	status=clReleaseMemObject(input2);
 	status=clReleaseMemObject(outputBuffer);
-
 	return 1;
 }
 
 
 static void CleanupCL(CLGoo *goo) {
 	cl_int status;
-
 	if (goo->queue != NULL) {
 		if ((status = clReleaseCommandQueue(goo->queue)) != CL_SUCCESS) {
 			Warning("clReleaseCommandQueue failed: %s", StrCLError(status));
 		}
 		goo->queue = NULL;
 	}
-
 	free(goo->devices);
 	goo->numDevices = 0;
-
 	if (goo->ctx != NULL) {
 		if ((status = clReleaseContext(goo->ctx)) != CL_SUCCESS) {
 			Warning("clReleaseContext failed: %s", StrCLError(status));
@@ -408,49 +372,50 @@ static void CleanupCL(CLGoo *goo) {
 //	const char *kernelName;
 //	const char *fileName;
 //	bool fromfile;
+//  bool quiet;
 //	int timing;
 //};
 int main(int argc, char * argv[])
 {
 	CLGoo goo;
+	
+	//Opts opts = {"",CL_DEVICE_TYPE_CPU,1000,"computemap","",false,false,true};
+	Opts opts = {"-DARRAY_SIZE=256*256",CL_DEVICE_TYPE_GPU,256,256,"computemap","convolution.cl",true,false,true};
+	
+	operand1=(struct point*)malloc(sizeof(struct point)*opts.array_size1);
+	operand2=(struct point*)malloc(sizeof(struct point)*opts.array_size2);
+	output=(struct point*)malloc(sizeof(struct point)*opts.array_size1*opts.array_size2);
 
-	//Opts opts = {"-DARRAY_SIZE=1000",CL_DEVICE_TYPE_CPU,1000,"computemap","convolution.cl",false,true,true};
-	Opts opts = {"-DARRAY_SIZE=100",CL_DEVICE_TYPE_GPU,100,"computemap","convolution.cl",true,false,true};
-
-	cendx1=(unsigned int*)malloc(sizeof(unsigned int)*opts.array_size);
-	cendx2=(unsigned int*)malloc(sizeof(unsigned int)*opts.array_size);
-	cvalue1=(unsigned int*)malloc(sizeof(unsigned int)*opts.array_size);
-	cvalue2=(unsigned int*)malloc(sizeof(unsigned int)*opts.array_size);
-	cstartx1=(unsigned int*)malloc(sizeof(unsigned int)*opts.array_size);
-	cstartx2=(unsigned int*)malloc(sizeof(unsigned int)*opts.array_size);
-	output=(unsigned int*)malloc(2*sizeof(unsigned int)*opts.array_size);
-
-	for (unsigned int i=0;i<opts.array_size;i++){
-		cvalue1[i]=i;
-		cvalue2[i]=i;
-		cstartx1[i]=i;
-		cstartx2[i]=i;
-		cendx1[i]=i+1;
-		cendx2[i]=i+1;
+	for (unsigned int i=0;i<opts.array_size1;i++){
+		operand1[i].endx=i*1.0f;
+		operand1[i].endy=i*1.0f;
+		operand1[i].midx=i*1.0f;
+		operand1[i].midy=i*1.0f;
+		operand1[i].startx=i*1.0f;
+		operand1[i].starty=i*1.0f;
+	}
+	for (unsigned int i=0;i<opts.array_size2;i++){
+		operand2[i].endx=i*1.0f;
+		operand2[i].endy=i*1.0f;
+		operand2[i].midx=i*1.0f;
+		operand2[i].midy=i*1.0f;
+		operand2[i].startx=i*1.0f;
+		operand2[i].starty=i*1.0f;
 	}
 	
 	if (!InitializeCL(&goo, &opts)) {
 		exit(1);
 	}
-
+	
 	if (!RunTest(&goo, &opts)) {
 		exit(1);
 	}
-
+	
 	CleanupCL(&goo);
-
+	
 	free(output);
-	free(cendx1);
-	free(cendx2);
-	free(cvalue1);
-	free(cvalue2);
-	free(cstartx1);
-	free(cstartx2);
-
+	free(operand1);
+	free(operand2);
+	
 	return 0;
 }
--- code/RTC Toolbox/convolution.cl
+++ code/RTC Toolbox/convolution.cl
@@ -1,26 +1,17 @@
+typedef struct {
+	float startx;
+	float starty;
+	float midx;
+	float midy;
+	float endx;
+	float endy;
+}point;
 
-__kernel void computemap(__global uint* startx1,__global uint* endx1,__global uint* y1,__global uint* startx2,
-											__global uint* endx2,__global uint* y2,__global uint* map){
-
-	__local volatile uint localmap[2*ARRAY_SIZE];
-
-	size_t xId=get_global_id(0);
-	size_t yId=get_global_id(1);
-
-	localmap[2*xId]=INFINITY;
-	localmap[2*xId+1]=INFINITY;
-	
-	barrier(CLK_LOCAL_MEM_FENCE);
-
-	uint val=y1[xId]+y2[yId];
-	uint endx=endx1[xId]+endx2[yId];
-	uint startx=startx1[xId]+startx2[yId];
-
-	for (int i=startx;i<=endx;i++)
-		atomic_min(localmap+i,val);
-	
-	barrier(CLK_LOCAL_MEM_FENCE);
-
-	map[2*xId]=localmap[2*xId];
-	map[2*xId+1]=localmap[2*xId+1];
+__kernel void computemap(__global point *operand1,const int num1,__global point *operand2,const int num2,const int stage,__global point *output){
+		size_t size=get_global_size(0);
+		size_t col_idx=get_global_id(1);
+		size_t row_idx=get_global_id(0);
+		size_t col_offset=get_global_offset(1);
+		size_t row_offset=get_global_offset(0);
+		size_t index=size*(row_idx-row_offset)+(col_idx-col_offset);
 }
